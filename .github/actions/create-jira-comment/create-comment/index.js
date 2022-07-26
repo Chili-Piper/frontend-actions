e436a7ad6cf9ac8222e3962a30a2fb8ef477b807
Alex Nekrasov <alexey@chilipiper.com>
2022-07-26 18:08:50 +0400

    CPP-8107 Add built create comment js action

--- .github/actions/create-jira-comment/create-comment/index.js
+++ .github/actions/create-jira-comment/create-comment/index.js
@@ -1,5 +1,20 @@
 const core = require('@actions/core');
-const { jira } = require("../../../../helpers/jira");
+
+const JiraApi = require('jira-client')
+
+const username = core.getInput('jira_username');
+const password = core.getInput('jira_password');
+
+const jira = new JiraApi({
+    protocol: 'https',
+    host: 'floatingapps.atlassian.net',
+    // https://id.atlassian.com/manage-profile/email
+    username,
+    // https://id.atlassian.com/manage-profile/security/api-tokens
+    password,
+    apiVersion: '2',
+    strictSSL: true,
+})
 
 const jiraTicket = core.getInput('jira_ticket');
 const cloudrunLink = core.getInput('cloudrunLink');

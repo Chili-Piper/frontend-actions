7895ba2183d7c5e3ddba13d82e1d04ace567e4dc
Alex Nekrasov <alexey@chilipiper.com>
2022-08-30 17:27:39 +0500

    CPP-8788 Fix set secret action
    Update encrypt helper

--- .github/actions/set-secret/index.js
+++ .github/actions/set-secret/index.js
@@ -9,16 +9,16 @@ const value = core.getInput('value');
 const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 
 const encrypt = async (repoPublicKey, secretValue) => {
-    // Convert the message and key to Uint8Array's (Buffer implements that interface)
-    const messageBytes = Buffer.from(secretValue);
-    const keyBytes = Buffer.from(repoPublicKey, 'base64');
+    // Convert Secret & Base64 key to Uint8Array
+    const binKey = libsodium.from_base64(repoPublicKey, libsodium.base64_variants.ORIGINAL)
+    const binSecret = libsodium.from_string(secretValue)
 
     // Encrypt using LibSodium.
     await libsodium.ready
-    const encryptedBytes = libsodium.crypto_box_seal(messageBytes, keyBytes)
+    const encryptedBytes = libsodium.crypto_box_seal(binSecret, binKey)
 
     // Base64 the encrypted secret
-    return Buffer.from(encryptedBytes).toString('base64');
+    return libsodium.to_base64(encryptedBytes, libsodium.base64_variants.ORIGINAL)
 }
 
 const getPublicKey = async () => {
@@ -51,10 +51,10 @@ const setSecretRequest = async (repoPublicKeyId, secretName, encryptedSecretValu
 })
 
 const setSecret = async (secretName, secretValue) => {
-    const {key_id, key} = await getPublicKey(repo)
+    const {key_id: keyId, key} = await getPublicKey(repo)
     const encryptedSecretValue = await encrypt(key, secretValue);
 
-    await setSecretRequest(key_id, secretName, encryptedSecretValue)
+    await setSecretRequest(keyId, secretName, encryptedSecretValue)
 }
 
 setSecret(name, value)

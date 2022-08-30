99a13933ed28f4cda96ea159aa11b6821b85ee01
Alex Nekrasov <alexey@chilipiper.com>
2022-08-30 15:13:52 +0500

    CPP-8788 Fix set secret action

--- .github/actions/set-secret/index.js
+++ .github/actions/set-secret/index.js
@@ -1,17 +1,12 @@
 const core = require('@actions/core');
 import libsodium from 'libsodium-wrappers'
-const Octokit = require('octokit')
+const { Octokit } = require("@octokit/action");
 
-const owner = 'Chili-Piper'
-
-const token = core.getInput('token');
-const repo = core.getInput('repo');
 const name = core.getInput('name');
 const value = core.getInput('value');
 
-const octokit = new Octokit({
-    auth: token
-})
+const octokit = new Octokit();
+const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 
 const encrypt = async (repoPublicKey, secretValue) => {
     // Convert the message and key to Uint8Array's (Buffer implements that interface)
@@ -26,18 +21,18 @@ const encrypt = async (repoPublicKey, secretValue) => {
     return Buffer.from(encryptedBytes).toString('base64');
 }
 
-const getPublicKey = (repo) => octokit.request('GET /repos/{owner}/{repo}/actions/secrets/public-key', {
-        owner,
-        repo
-    })
+const getPublicKey = () => octokit.request('GET /repos/{owner}/{repo}/actions/secrets/public-key', {
+    owner,
+    repo
+})
 
-const setSecretRequest = async (repo, repoPublicKey, secretName, encryptedSecretValue) => octokit.request('PUT /repos/{owner}/{repo}/actions/secrets/{secret_name}', {
-        owner,
-        repo,
-        secret_name: secretName,
-        encrypted_value: encryptedSecretValue,
-        key_id: repoPublicKey
-    })
+const setSecretRequest = async (repoPublicKey, secretName, encryptedSecretValue) => octokit.request('PUT /repos/{owner}/{repo}/actions/secrets/{secret_name}', {
+    owner,
+    repo,
+    secret_name: secretName,
+    encrypted_value: encryptedSecretValue,
+    key_id: repoPublicKey
+})
 
 const setSecret = async (repo, secretName, secretValue) => {
     const repoPublicKey = await getPublicKey(repo)
